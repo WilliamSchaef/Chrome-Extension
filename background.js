33550feb59d2a4be646e1dbf9e63e40a0c0b4806
WilliamSchaef <81344245+WilliamSchaef@users.noreply.github.com>
2021-03-25 01:28:59 -0600

    Update background.js

--- background.js
+++ background.js
@@ -1,13 +1,1 @@
-
-let active_tab_id = 0;
-
-chrome.tabs.onActivated.addListener(tab => {
-    chrome.tabs.get(tab.tabId, current_tab_info => {
-        active_tab_id = tab.tabId;
-
-        if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
-            chrome.tabs.insertCSS(null, { file: './popupstyles.css' });
-            chrome.tabs.executeScript(null, { file: './foreground.js' }, () => console.log('Sorry I had to intervene'))
-        }
-    });
-});
+//background js that needs to refer to foreground to actually access info on the site
