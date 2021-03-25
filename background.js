2d5acb00cd8262a1657bf680ccbd701bf4cee749
WilliamSchaef <81344245+WilliamSchaef@users.noreply.github.com>
2021-03-25 01:26:13 -0600

    Add files via upload

--- background.js
+++ background.js
@@ -1,1 +1,13 @@
-//need code
+
+let active_tab_id = 0;
+
+chrome.tabs.onActivated.addListener(tab => {
+    chrome.tabs.get(tab.tabId, current_tab_info => {
+        active_tab_id = tab.tabId;
+
+        if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
+            chrome.tabs.insertCSS(null, { file: './popupstyles.css' });
+            chrome.tabs.executeScript(null, { file: './foreground.js' }, () => console.log('Sorry I had to intervene'))
+        }
+    });
+});
